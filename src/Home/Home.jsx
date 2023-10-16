c546d3be5f60c3dc00ac87c6dc2c1bcbd4719cf8
voiiddxx <ursfan8102003@gmail.com>
2023-10-16 16:51:43 +0530

    last changes

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Nav from '../components/Nav/Nav'
 import Hero from '../components/hero/Hero'
+import "./Home.css"
+import aboutmain from "./aboutmain.svg"
 
 const Home = () => {
 
@@ -10,7 +12,18 @@ const Home = () => {
     <>
     <Nav/>
     <Hero/>
-
+    <div className="about">
+    <div className="about-text">
+    <h1>Get To Know About The Pro Planet person Platform</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem illum officiis nobis saepe reprehenderit quae quam facilis <br /> ut, non iusto consequatur velit dolorum earum assumenda ipsa iste alias. Odit tempora cumque doloremque! Tenetur nisi, optio nostrum natus doloremque consequuntur ad necessitatibus vero nam eaque aspernatur! <br /> Eveniet veniam minima pariatur laudantium non inventore voluptates maxime molestiae ex? Ratione delectus praesentium nobis!</p>
+    </div>
+    <div className="about-image">
+      <img src={aboutmain} alt="aboutmain" />
+    </div>
+    </div>
+    <div className="footer">
+      <p>Created By Nikhil Kumar 💚</p>
+    </div>
     </>
   )
 }
